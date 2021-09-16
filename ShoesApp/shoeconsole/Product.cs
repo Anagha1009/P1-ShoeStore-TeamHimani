47b9edead70e73883306f785300384f68fff7d0e
Anagha Thorat <t.anagha8@gmail.com>
2021-09-17 00:26:53 +0530

    new changes in console

--- ShoesApp/shoeconsole/Product.cs
+++ ShoesApp/shoeconsole/Product.cs
@@ -14,9 +14,9 @@ namespace shoeconsole
         public string Category_Name { get; set; }
         public int Store_Id { get; set; }
         public string Store_Name { get; set; }
-
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public string Image { get; set; }
+        public bool Type { get; set; }
     }
 }
